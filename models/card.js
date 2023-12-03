5fa454e9af9be50fb1115e15b4a4292dbb37c2c1
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-12-03 18:46:13 +0500

    Update card.js

--- models/card.js
+++ models/card.js
@@ -17,8 +17,12 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   likes: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
     default: [],
   },
   createdAt: {

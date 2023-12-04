7eb70b47ed48815f34ea2811bfe26eefb7119085
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-12-04 23:48:02 +0500

    финальные правкии

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,5 @@
 const { ERROR_INTERNAL_SERVER } = require('../errors/errors');
 
-// module.exports.errorHandle = (err, req, res, next) => {
-//   res.status(err.statusCode).send({ message: err.message });
-// };
-
 module.exports.errorHandle = (err, req, res, next) => {
   const { statusCode = ERROR_INTERNAL_SERVER } = err;
   const message =
